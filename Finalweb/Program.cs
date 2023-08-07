d3d72579c12769803b16106b83f5bf9fbdc09d4e
bhavya123654 <128205153+bhavya123654@users.noreply.github.com>
2023-08-07 13:32:13 -0400

    Update Program.cs

--- Finalweb/Program.cs
+++ Finalweb/Program.cs
@@ -24,4 +24,16 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+// Your custom logic to display the database access message
+app.Use(async (context, next) =>
+{
+    // Display the database access message
+    string currentDate = DateTime.Now.ToString("08 07, 2023");
+    string message = $"Database access has already been done on 2023-08-07 by Bhavitha Penaka"; 
+    await context.Response.WriteAsync(message);
+
+    // Call the next middleware in the pipeline
+    await next();
+});
+
 app.Run();
