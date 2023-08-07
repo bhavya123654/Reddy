f519fb4daac15ff6ff833293b938e7806e8f2d9c
bhavya123654 <128205153+bhavya123654@users.noreply.github.com>
2023-08-07 13:49:02 -0400

    Database

--- Finalweb/Program.cs
+++ Finalweb/Program.cs
@@ -1,39 +1,15 @@
-var builder = WebApplication.CreateBuilder(args);
+using System;
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+namespace Finalweb
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string currentDate = new DateTime(2023, 4, 14).ToString("MMMM dd, yyyy");
+            string message = $"Database access has already been done on April 14, 2023 by Bhavitha Penaka";
+
+            Console.WriteLine(message);
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-// Your custom logic to display the database access message
-app.Use(async (context, next) =>
-{
-    // Display the database access message
-    string currentDate = DateTime.Now.ToString("08 07, 2023");
-    string message = $"Database access has already been done on 2023-08-07 by Bhavitha Penaka"; 
-    await context.Response.WriteAsync(message);
-
-    // Call the next middleware in the pipeline
-    await next();
-});
-
-app.Run();
